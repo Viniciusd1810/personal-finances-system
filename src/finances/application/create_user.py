b53e49c31b34410acd8578875ee2bfee0e3feba8
Viniciusd1810 <viniciusd1810@gmail.com>
2026-09-14 21:24:53 -0300

    refactor: decouple CreateUser from password hasher implementation~

--- src/finances/application/create_user.py
+++ src/finances/application/create_user.py
@@ -1,9 +1,11 @@
-from domain.user import User
-from infrastructure.security import password_hasher
+from finances.domain.user import User
 
 class CreateUser:
+    def __init__(self, password_hasher):
+        self._password_hasher = password_hasher
+
     def execute(self, email, user_password):
-        password_hash = password_hasher.hash_password(user_password)
+        password_hash = self._password_hasher.hash_password(user_password)
 
         new_user = User(email,password_hash)
         return new_user
