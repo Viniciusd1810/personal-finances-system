c573d31f2ef9f2e18ba679e946e90ac7be77ca7a
Viniciusd1810 <viniciusd1810@gmail.com>
2026-09-16 22:55:04 -0300

    tests: create user new tests -tests to validate user_password -tests to validate the dependency injection

--- src/finances/application/create_user.py
+++ src/finances/application/create_user.py
@@ -5,7 +5,15 @@ class CreateUser:
         self._password_hasher = password_hasher
 
     def execute(self, email, user_password):
+        if not isinstance(user_password,str):
+            raise TypeError("User password must be a string")
+        if user_password.strip() == "":
+            raise ValueError("User password cannot be blank")
+        if " " in user_password:
+            raise ValueError("User password cannot have blank spaces")
+        if len(user_password) < 8:
+            raise ValueError("User password cannot have less than 8 characters")
+        
         password_hash = self._password_hasher.hash_password(user_password)
-
         new_user = User(email,password_hash)
         return new_user
--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -2,16 +2,70 @@ import pytest
 from finances.application.create_user import CreateUser
 from finances.domain.user import User
 
+# Fake password hasher to test dependency injection
+
 class FakePasswordHasher():
+    def __init__(self):
+        self.received_password = None
     def hash_password(self, test_password):
+        self.received_password = test_password
         return "fake_hash"
-
-Fake_Hash = FakePasswordHasher()
-
+    
 def test_create_user_returns_user():
-    create = CreateUser(Fake_Hash)
+    FakeHash = FakePasswordHasher()
+
+    create = CreateUser(FakeHash)
 
     result = create.execute(email="test@email.com",
                             user_password="pass_test"
                             )
     assert isinstance(result, User)
+
+def test_create_user_password_cannot_be_blank():
+    FakeHash = FakePasswordHasher()
+    with pytest.raises(ValueError):
+        create = CreateUser(FakeHash)
+        create.execute(email="test@email.com",
+                        user_password="")
+
+def test_create_user_password_cannot_contain_only_spaces():
+    FakeHash = FakePasswordHasher()
+    with pytest.raises(ValueError):
+            create = CreateUser(FakeHash)
+            create.execute(email="test@email.com",
+                            user_password="      ")
+
+def test_create_user_password_cannot_contains_blank_spaces():
+    FakeHash = FakePasswordHasher()
+    with pytest.raises(ValueError):
+            create = CreateUser(FakeHash)
+            create.execute(email="test@email.com",
+                            user_password="1234   8")
+
+def test_create_user_password_cannot_have_less_than_8_characters():
+    FakeHash = FakePasswordHasher()
+    with pytest.raises(ValueError):
+            create = CreateUser(FakeHash)
+            create.execute(email="test@email.com",
+                            user_password="1234567")
+
+def test_create_user_password_must_be_string():
+    FakeHash = FakePasswordHasher()
+    with pytest.raises(TypeError):
+        create = CreateUser(FakeHash)
+        create.execute(email="test@email.com",
+                        user_password=12345678)
+
+def test_create_user_hash_password_works():
+    FakeHash = FakePasswordHasher()
+    create = CreateUser(FakeHash)
+    user = create.execute(email="test@email.com",
+                    user_password="test_hash")
+    assert user._password_hash == "fake_hash"
+
+def test_create_user_password_arrives_at_hash_password():
+    FakeHash = FakePasswordHasher()
+    create = CreateUser(FakeHash)
+    create.execute(email="test@email.com",
+                    user_password="test_hash")
+    assert FakeHash.received_password == "test_hash"
