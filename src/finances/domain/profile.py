2ce8979b71768a1c8db37e8e1634fca2bb4b9c7b
Viniciusd1810 <viniciusd1810@gmail.com>
2026-09-22 21:06:15 -0300

    chore: fixed interface from profile.name and rename variable to more natural reading

--- src/finances/domain/profile.py
+++ src/finances/domain/profile.py
@@ -1,9 +1,14 @@
 class Profile:
-    def __init__(self, profile_name, currency):
-        if not isinstance(profile_name, str):
-            raise TypeError("profile_name cannot be this type")
-        if profile_name.strip() == "":
-            raise ValueError("profile_name cannot be blank")
-        self._profile_name = profile_name
-        
+    def __init__(self, name, currency):
+        if not isinstance(name, str):
+            raise TypeError("name cannot be this type")
+        if name.strip() == "":
+            raise ValueError("name cannot be blank")
+        self._name = name
+
         self._currency = currency
+
+    @property
+    def name(self):
+        return self._name
+        
--- tests/test_profile.py
+++ tests/test_profile.py
@@ -2,13 +2,13 @@ import pytest
 from finances.domain.profile import Profile
 
 def test_profile_has_name():
-    profile = Profile(profile_name="profile_test",currency="BRL")
-    assert profile._profile_name == "profile_test"
+    profile = Profile(name="profile_test",currency="BRL")
+    assert profile.name == "profile_test"
 
 def test_profile_cannot_have_empty_name():
     with pytest.raises(ValueError):
-        Profile(profile_name="",currency="BRL")
+        Profile(name="",currency="BRL")
 
 def test_profile_name_cannot_be_int():
     with pytest.raises(TypeError):
-        Profile(profile_name=1234,currency="BRL")
+        Profile(name=1234,currency="BRL")
